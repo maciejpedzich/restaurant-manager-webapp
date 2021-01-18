cb6b45f19009bcb9e5ec0968800077af7d361e3e
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-18 21:47:01 +0100

    Add axios config and use ToastService

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
+import axios from 'axios';
 
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 import 'primevue/resources/primevue.min.css';
 import 'primeflex/primeflex.css';
 import 'primevue/resources/themes/saga-green/theme.css';
@@ -9,9 +11,13 @@ import 'primeicons/primeicons.css';
 import App from './App.vue';
 import router from './router';
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+axios.defaults.withCredentials = true;
+
 const app = createApp(App);
 
 app
   .use(PrimeVue)
+  .use(ToastService)
   .use(router)
   .mount('#app');
