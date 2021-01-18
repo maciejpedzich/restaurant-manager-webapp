52b8f7471768a98be0529bf67ca4ecfcb152f06c
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-18 12:50:02 +0100

    Install and use PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
 
-createApp(App).use(router).mount('#app')
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-green/theme.css';
+import 'primeicons/primeicons.css';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app
+  .use(PrimeVue)
+  .use(router)
+  .mount('#app');
