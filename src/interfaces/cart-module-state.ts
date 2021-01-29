7114c146a4e1fb5401a51206808098415bd2b04f
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-29 22:14:58 +0100

    Create Vuex order module interfaces

---
+++ src/interfaces/cart-module-state.ts
@@ -0,0 +1,5 @@
+import OrderProduct from '@/interfaces/order-product';
+
+export default interface CartModuleState {
+  products: OrderProduct[];
+}
---
+++ src/interfaces/order-product.ts
@@ -0,0 +1,5 @@
+export default interface OrderProduct {
+  name: string;
+  cost: number;
+  quantity: number;
+}
